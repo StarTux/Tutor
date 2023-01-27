dec9b4994a939e53b35ce34f7977c2795e13b12f
StarTux <ths.loy@googlemail.com>
2023-01-27 20:55:15 +0100

    Formatting and bdays

--- src/main/java/com/cavetale/tutor/goal/FriendsGoal.java
+++ src/main/java/com/cavetale/tutor/goal/FriendsGoal.java
@@ -2,7 +2,6 @@ package com.cavetale.tutor.goal;
 
 import com.cavetale.core.event.player.PluginPlayerEvent.Detail;
 import com.cavetale.core.event.player.PluginPlayerEvent;
-import com.cavetale.core.event.player.PluginPlayerQuery;
 import com.cavetale.core.font.Unicode;
 import com.cavetale.core.font.VanillaItems;
 import com.cavetale.mytems.Mytems;
@@ -10,8 +9,9 @@ import com.cavetale.tutor.session.PlayerQuest;
 import java.util.List;
 import lombok.Getter;
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.JoinConfiguration;
-import net.kyori.adventure.text.format.NamedTextColor;
+import static net.kyori.adventure.text.Component.text;
+import static net.kyori.adventure.text.Component.textOfChildren;
+import static net.kyori.adventure.text.format.NamedTextColor.*;
 
 public final class FriendsGoal extends AbstractGoal<FriendsProgress> {
     protected static final int GIFTS = 3;
@@ -23,160 +23,134 @@ public final class FriendsGoal extends AbstractGoal<FriendsProgress> {
     protected final CheckboxCondition condList;
     protected final NumberCondition condGift;
     protected final CheckboxCondition condPriest;
-    protected final CheckboxCondition condBday;
 
     public FriendsGoal() {
         super(FriendsProgress.class, FriendsProgress::new);
         this.id = "friends";
-        this.displayName = Component.text("Making Friends");
-        condList = new CheckboxCondition(Component.text("View your friends list"),
+        this.displayName = text("Making Friends");
+        condList = new CheckboxCondition(text("View your friends list"),
                                          playerQuest -> getProgress(playerQuest).list,
                                          playerQuest -> getProgress(playerQuest).list = true);
-        condGift = new NumberCondition(Component.text("Share Friendship Gifts"), GIFTS,
+        condGift = new NumberCondition(text("Share Friendship Gifts"), GIFTS,
                                        playerQuest -> getProgress(playerQuest).gifts,
                                        (playerQuest, amount) -> getProgress(playerQuest).gifts = amount);
-        condPriest = new CheckboxCondition(Component.text("Find the Priest"),
+        condPriest = new CheckboxCondition(text("Find the Priest"),
                                            playerQuest -> getProgress(playerQuest).priest,
                                            playerQuest -> getProgress(playerQuest).priest = true);
-        condBday = new CheckboxCondition(Component.text("Share your Birthday"),
-                                         playerQuest -> getProgress(playerQuest).bday,
-                                         playerQuest -> getProgress(playerQuest).bday = true);
         condList.setBookPageIndex(0);
         condGift.setBookPageIndex(1);
         condPriest.setBookPageIndex(5);
-        condBday.setBookPageIndex(7);
-        this.conditions = List.of(new Condition[] {
-                condList,
-                condGift,
-                condPriest,
-                condBday,
-            });
+        this.conditions = List.of(condList,
+                                  condGift,
+                                  condPriest);
         this.constraints = List.of();
         this.additionalBookPages = List.of(new Component[] {
-                Component.join(JoinConfiguration.noSeparators(), new Component[] {// 0
-                        Component.text("You can grow your friendship with every other player"
-                                       + " on Cavetale."
-                                       + "\n\nCommand:\n"),
-                        Component.text("/friends", NamedTextColor.BLUE),
-                        Component.text("\nView your friends list, along with the daily friendship gift",
-                                       NamedTextColor.GRAY),
-                    }),
-                Component.join(JoinConfiguration.noSeparators(), new Component[] {// 1
-                        Component.text("To gain friendship with somebody, drop the "),
-                        Component.text("friendship gift", NamedTextColor.BLUE),
-                        Component.text(" so that they pick it up."),
-                        Component.text("\n\nYou can find the gift on the "),
-                        Component.text("/friends", NamedTextColor.BLUE),
-                        Component.text(" list."
-                                       + "\n\nIt changes every day and includes:"),
-                        VanillaItems.MELON_SLICE.component,
-                        VanillaItems.APPLE.component,
-                        VanillaItems.COOKIE.component,
-                        VanillaItems.PUMPKIN_PIE.component,
-                        VanillaItems.SWEET_BERRIES.component,
-                        VanillaItems.CAKE.component,
-                        VanillaItems.GOLDEN_APPLE.component,
-                    }),
-                Component.join(JoinConfiguration.noSeparators(), new Component[] {// 2
-                        Component.text("There are many ways to increase friendship with each other:\n"),
-                        Component.text("\n" + Unicode.BULLET_POINT.character),
-                        Component.text(" Daily friendship gift"),
-                        Component.text("\n" + Unicode.BULLET_POINT.character),
-                        Component.text(" Bonus for gifting on birthdays"),
-                        Component.text("\n" + Unicode.BULLET_POINT.character),
-                        Component.text(" Play server events"),
-                        Component.text("\n" + Unicode.BULLET_POINT.character),
-                        Component.text(" Complete raids"),
-                    }),
-                Component.join(JoinConfiguration.noSeparators(), new Component[] {// 3
-                        Component.text("At "),
-                        Component.text("3" + Unicode.HEART.character, NamedTextColor.RED),
-                        Component.text(", you can send a "),
-                        Component.text("Friend", NamedTextColor.BLUE),
-                        Component.text(" request."
-                                       + "\n\nCommand:"),
-                        Component.text("/friend <player>", NamedTextColor.BLUE),
-                        Component.text("\nSend a friend request."
-                                       + " They can accept by clicking"
-                                       + " the message in chat.", NamedTextColor.GRAY),
-                    }),
-                Component.join(JoinConfiguration.noSeparators(), new Component[] {// 4
-                        Component.text("Making "),
-                        Component.text("Friends", NamedTextColor.BLUE),
-                        Component.text(" has its perks:\n"),
-                        Component.text("\n" + Unicode.BULLET_POINT.character),
-                        Component.text(" Bonus hearts when starting a raid together"),
-                        Component.text("\n" + Unicode.BULLET_POINT.character),
-                        Component.text(" Friendship never drops below "),
-                        Component.text("2" + Unicode.HEART.character, NamedTextColor.RED),
-                    }),
-                Component.join(JoinConfiguration.noSeparators(), new Component[] {// 5
-                        Component.text("At "),
-                        Component.text("5" + Unicode.HEART.character, NamedTextColor.RED),
-                        Component.text(", you can ask to get "),
-                        Component.text("married", NamedTextColor.BLUE),
-                        Component.text(". All you need is two "),
-                        Mytems.WEDDING_RING.component,
-                        Component.text("Wedding Rings."
-                                       + "\n\nThe priest in the cathedral at spawn"
-                                       + " will sell them to you."
-                                       + " Can you find them?"),
-                    }),
-                Component.join(JoinConfiguration.noSeparators(), new Component[] {// 6
-                        Component.text("Marriage", NamedTextColor.BLUE),
-                        Component.text(" comes with perks:\n"),
-                        Component.text("\n" + Unicode.BULLET_POINT.character),
-                        Component.text(" Share hunger points when eating"),
-                        Component.text("\n" + Unicode.BULLET_POINT.character),
-                        Component.text(" Use the "),
-                        Component.text("/love", NamedTextColor.BLUE),
-                        Component.text(" command to share your love"),
-                    }),
-                Component.join(JoinConfiguration.noSeparators(), new Component[] {// 7
-                        Component.text("If you share your birthday,"
-                                       + " it will be visible on everyone's"
-                                       + " profile page as well as the website,"
-                                       + " unless you wish to keep it a secret."
-                                       + "\n\nCommand:\n"),
-                        Component.text("/profile", NamedTextColor.BLUE),
-                        Component.text("\nView your profile,"
-                                       + " where you can set your birthday"
-                                       + " by clicking the ", NamedTextColor.GRAY),
-                        Mytems.STAR.component,
-                    }),
+                // 0
+                textOfChildren(text("You can grow your friendship with every other player"
+                                    + " on Cavetale."
+                                    + "\n\nCommand:\n"),
+                               text("/friends", BLUE),
+                               text("\nView your friends list, along with the daily friendship gift",
+                                    GRAY)),
+                // 1
+                textOfChildren(text("To gain friendship with somebody, drop the "),
+                               text("friendship gift", BLUE),
+                               text(" so that they pick it up."),
+                               text("\n\nYou can find the gift on the "),
+                               text("/friends", BLUE),
+                               text(" list."
+                                    + "\n\nIt changes every day and includes:"),
+                               VanillaItems.MELON_SLICE.component,
+                               VanillaItems.APPLE.component,
+                               VanillaItems.COOKIE.component,
+                               VanillaItems.PUMPKIN_PIE.component,
+                               VanillaItems.SWEET_BERRIES.component,
+                               VanillaItems.CAKE.component,
+                               VanillaItems.GOLDEN_APPLE.component),
+                // 2
+                textOfChildren(text("There are many ways to increase friendship with each other:\n"),
+                               text("\n" + Unicode.BULLET_POINT.character),
+                               text(" Daily friendship gift"),
+                               text("\n" + Unicode.BULLET_POINT.character),
+                               text(" Bonus for gifting on birthdays"),
+                               text("\n" + Unicode.BULLET_POINT.character),
+                               text(" Play server events"),
+                               text("\n" + Unicode.BULLET_POINT.character),
+                               text(" Complete raids")),
+                // 3
+                textOfChildren(text("At "),
+                               text("3" + Unicode.HEART.character, RED),
+                               text(", you can send a "),
+                               text("Friend", BLUE),
+                               text(" request."
+                                    + "\n\nCommand:"),
+                               text("/friend <player>", BLUE),
+                               text("\nSend a friend request."
+                                    + " They can accept by clicking"
+                                    + " the message in chat.", GRAY)),
+                // 4
+                textOfChildren(text("Making "),
+                               text("Friends", BLUE),
+                               text(" has its perks:\n"),
+                               text("\n" + Unicode.BULLET_POINT.character),
+                               text(" Bonus hearts when starting a raid together"),
+                               text("\n" + Unicode.BULLET_POINT.character),
+                               text(" Friendship never drops below "),
+                               text("2" + Unicode.HEART.character, RED)),
+                // 5
+                textOfChildren(text("At "),
+                               text("5" + Unicode.HEART.character, RED),
+                               text(", you can ask to get "),
+                               text("married", BLUE),
+                               text(". All you need is two "),
+                               Mytems.WEDDING_RING.component,
+                               text("Wedding Rings."
+                                    + "\n\nThe priest in the cathedral at spawn"
+                                    + " will sell them to you."
+                                    + " Can you find them?")),
+                // 6
+                textOfChildren(
+                               text("Marriage", BLUE),
+                               text(" comes with perks:\n"),
+                               text("\n" + Unicode.BULLET_POINT.character),
+                               text(" Share hunger points when eating"),
+                               text("\n" + Unicode.BULLET_POINT.character),
+                               text(" Use the "),
+                               text("/love", BLUE),
+                               text(" command to share your love")),
+                // 7
+                textOfChildren(text("If you share your birthday,"
+                                    + " it will be visible on everyone's"
+                                    + " profile page as well as the website,"
+                                    + " unless you wish to keep it a secret."
+                                    + "\n\nCommand:\n"),
+                               text("/profile", BLUE),
+                               text("\nView your profile,"
+                                    + " where you can set your birthday"
+                                    + " by clicking the ", GRAY),
+                               Mytems.STAR.component),
             });
     }
 
     @Override
     public void onEnable(PlayerQuest playerQuest) {
-        checkPassiveProgress(playerQuest);
         if (!getProgress(playerQuest).isComplete()) {
             playerQuest.getSession().applyPet(pet -> {
                     pet.addSpeechBubble(id, 50L, 100L, new Component[] {
-                            Component.text("What's more important"),
-                            Component.text("in life than friends?"),
+                            text("What's more important"),
+                            text("in life than friends?"),
                         });
                     pet.addSpeechBubble(id, 0L, 100L, new Component[] {
-                            Component.text("Let's find out how to"),
-                            Component.text("make friends on Cavetale!"),
+                            text("Let's find out how to"),
+                            text("make friends on Cavetale!"),
                         });
                 });
-        }
-    }
-
-    void checkPassiveProgress(PlayerQuest playerQuest) {
-        FriendsProgress progress = getProgress(playerQuest);
-        if (PluginPlayerQuery.Name.DID_ENTER_BIRTHDAY.call(playerQuest.getPlugin(), playerQuest.getPlayer(), false)) {
-            condBday.progress(playerQuest);
         }
     }
 
     @Override
     public void onPluginPlayer(PlayerQuest playerQuest, PluginPlayerEvent event) {
         switch (event.getName()) {
-        case ENTER_BIRTHDAY:
-            condBday.progress(playerQuest);
-            break;
         case VIEW_FRIENDS_LIST:
             condList.progress(playerQuest);
             break;
@@ -186,11 +160,6 @@ public final class FriendsGoal extends AbstractGoal<FriendsProgress> {
         case INTERACT_NPC:
             if (Detail.NAME.is(event, "WeddingRing")) {
                 condPriest.progress(playerQuest);
-            }
-            break;
-        case PLAYER_SESSION_LOADED:
-            if (event.getPlugin().getName().equals("Fam")) {
-                checkPassiveProgress(playerQuest);
             }
             break;
         default: break;
@@ -203,12 +172,10 @@ final class FriendsProgress extends GoalProgress {
     protected boolean list;
     protected int gifts;
     protected boolean priest;
-    protected boolean bday;
 
     @Override
     public boolean isComplete() {
-        return bday
-            && list
+        return list
             && gifts >= FriendsGoal.GIFTS
             && priest;
     }
