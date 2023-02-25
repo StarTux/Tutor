54cec68df003d35a857e10fb1f4d38d948e44bb8
StarTux <ths.loy@googlemail.com>
2023-02-25 02:20:00 +0100

    Rebalance mob kill rewards

--- src/main/java/com/cavetale/tutor/daily/DailyQuestKillMonster.java
+++ src/main/java/com/cavetale/tutor/daily/DailyQuestKillMonster.java
@@ -4,8 +4,9 @@ import com.cavetale.core.connect.NetworkServer;
 import com.cavetale.core.font.Unicode;
 import com.cavetale.mytems.Mytems;
 import java.util.List;
+import java.util.Random;
 import java.util.Set;
-import java.util.function.Supplier;
+import java.util.function.Function;
 import lombok.RequiredArgsConstructor;
 import net.kyori.adventure.text.Component;
 import org.bukkit.GameMode;
@@ -29,41 +30,50 @@ public final class DailyQuestKillMonster extends DailyQuest<DailyQuestKillMonste
      * The reward amount will be multiplied with the mob amount.
      */
     public enum TargetMob {
-        CREEPER("Creepers", 5, () -> new ItemStack(Material.GUNPOWDER, 5),
+        CREEPER("Creepers", 5, rnd -> new ItemStack(Material.GUNPOWDER, 64),
                 Mytems.CREEPER_FACE, Set.of(EntityType.CREEPER)),
-        ENDERMAN("Endermen", 3, () -> new ItemStack(Material.ENDER_PEARL, 5),
+        ENDERMAN("Endermen", 3, rnd -> new ItemStack(Material.ENDER_PEARL, 16),
                  Mytems.ENDERMAN_FACE, Set.of(EntityType.ENDERMAN)),
-        GHAST("Ghasts", 2, () -> new ItemStack(Material.GHAST_TEAR, 2),
+        GHAST("Ghasts", 2, rnd -> new ItemStack(Material.GHAST_TEAR, 16),
               Mytems.GHAST_FACE, Set.of(EntityType.GHAST)),
-        SKELETON("Skeleton", 5, () -> new ItemStack(Material.BONE, 5),
+        SKELETON("Skeleton", 5, rnd -> new ItemStack(Material.BONE, 64),
                  Mytems.SKELETON_FACE, Set.of(EntityType.SKELETON, EntityType.WITHER_SKELETON, EntityType.STRAY)),
-        SLIME("Slimes", 7, () -> new ItemStack(Material.SLIME_BALL, 3),
+        SLIME("Slimes", 7, rnd -> new ItemStack(Material.SLIME_BALL, 64),
               Mytems.SLIME_FACE, Set.of(EntityType.SLIME)),
-        SPIDER("Spider", 5, () -> new ItemStack(Material.STRING, 5),
+        SPIDER("Spider", 5, rnd -> new ItemStack((rnd.nextBoolean() ? Material.STRING : Material.SPIDER_EYE), 64),
                Mytems.SPIDER_FACE, Set.of(EntityType.SPIDER, EntityType.CAVE_SPIDER)),
-        PILLAGER("Pillager", 1, () -> new ItemStack(Material.ARROW, 5),
+        PILLAGER("Pillager", 1, rnd -> new ItemStack(Material.ARROW, 64),
                  Mytems.PILLAGER_FACE, Set.of(EntityType.PILLAGER, EntityType.VINDICATOR, EntityType.EVOKER)),
-        ZOMBIE("Zombie", 5, () -> new ItemStack(Material.IRON_INGOT, 3),
+        ZOMBIE("Zombie", 5, rnd -> new ItemStack(Material.IRON_INGOT, 64),
                Mytems.ZOMBIE_FACE, Set.of(EntityType.ZOMBIE, EntityType.DROWNED, EntityType.ZOMBIE_VILLAGER, EntityType.HUSK)),
-        WITCH("Witches", 3, () -> new ItemStack(Material.GLOWSTONE_DUST, 3),
-              Mytems.WITCH_FACE, Set.of(EntityType.WITCH)),
-        BLAZE("Blazes", 3, () -> new ItemStack(Material.BLAZE_POWDER, 5),
+        WITCH("Witches", 3, rnd -> switch (rnd.nextInt(6)) {
+            case 0 -> new ItemStack(Material.GLOWSTONE_DUST, 64);
+            case 1 -> new ItemStack(Material.GLASS_BOTTLE, 64);
+            case 2 -> new ItemStack(Material.REDSTONE, 64);
+            case 3 -> new ItemStack(Material.GUNPOWDER, 64);
+            case 4 -> new ItemStack(Material.SPIDER_EYE, 64);
+            case 5 -> new ItemStack(Material.SUGAR, 64);
+            default -> new ItemStack(Material.STICK, 64);
+            }, Mytems.WITCH_FACE, Set.of(EntityType.WITCH)),
+        BLAZE("Blazes", 3, rnd -> new ItemStack(Material.BLAZE_ROD, 64),
               Mytems.BLAZE_FACE, Set.of(EntityType.BLAZE)),
-        PIGLIN("Piglins", 3, () -> new ItemStack(Material.GOLD_INGOT, 5),
+        PIGLIN("Piglins", 3, rnd -> new ItemStack(Material.GOLD_INGOT, 64),
                Mytems.PIGLIN_FACE, Set.of(EntityType.PIGLIN, EntityType.ZOMBIFIED_PIGLIN, EntityType.PIGLIN_BRUTE)),
-        GUARDIAN("Guardians", 3, () -> new ItemStack(Material.SPONGE, 5),
+        GUARDIAN("Guardians", 3, rnd -> new ItemStack(Material.WET_SPONGE, 64),
                  Mytems.GUARDIAN_FACE, Set.of(EntityType.GUARDIAN)),
-        PHANTOM("Phantom", 1, () -> new ItemStack(Material.PHANTOM_MEMBRANE, 5),
+        PHANTOM("Phantoms", 3, rnd -> new ItemStack(Material.PHANTOM_MEMBRANE, 16),
                 Mytems.PHANTOM_FACE, Set.of(EntityType.PHANTOM)),
-        MAGMA_CUBE("Magma Cubes", 5, () -> new ItemStack(Material.MAGMA_CREAM, 5),
+        MAGMA_CUBE("Magma Cubes", 5, rnd -> new ItemStack(Material.MAGMA_CREAM, 64),
                    Mytems.MAGMA_CUBE_FACE, Set.of(EntityType.MAGMA_CUBE)),
-        WITHER_SKELETON("Wither Skeletons", 3, () -> new ItemStack(Material.COAL, 5),
+        WITHER_SKELETON("Wither Skeletons", 3, rnd -> rnd.nextBoolean()
+                        ? new ItemStack(Material.COAL, 64)
+                        : new ItemStack(Material.WITHER_SKELETON_SKULL, 3),
                         Mytems.WITHER_SKELETON_FACE, Set.of(EntityType.WITHER_SKELETON)),
         ;
 
         public final String displayName;
         public final int total;
-        public final Supplier<ItemStack> rewardSupplier;
+        public final Function<Random, ItemStack> rewardsFunction;
         public final Mytems chatIcon;
         public final Set<EntityType> entityTypes;
     }
@@ -104,7 +114,7 @@ public final class DailyQuestKillMonster extends DailyQuest<DailyQuestKillMonste
 
     @Override
     protected List<ItemStack> generateRewards() {
-        ItemStack result = details.target.rewardSupplier.get();
+        ItemStack result = details.target.rewardsFunction.apply(random);
         result.setAmount(Math.min(result.getType().getMaxStackSize(), result.getAmount() * total));
         return List.of(result);
     }
