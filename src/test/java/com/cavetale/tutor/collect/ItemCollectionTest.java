9842e7f54651b85375a83d57d29430bacdfb57a1
StarTux <ths.loy@googlemail.com>
2023-02-28 21:29:45 +0100

    Add test class

---
+++ src/test/java/com/cavetale/tutor/collect/ItemCollectionTest.java
@@ -0,0 +1,24 @@
+package com.cavetale.tutor.collect;
+
+import java.util.EnumMap;
+import org.junit.Test;
+
+public final class ItemCollectionTest {
+    @Test
+    public void test() {
+        EnumMap<ItemCollectionType, Integer> dependingMap = new EnumMap<>(ItemCollectionType.class);
+        for (ItemCollectionType type : ItemCollectionType.values()) {
+            for (ItemCollectionType dependency : type.dependencies) {
+                int depending = dependingMap.getOrDefault(dependency, 0);
+                dependingMap.put(dependency, depending + 1);
+            }
+        }
+        for (ItemCollectionType type : ItemCollectionType.values()) {
+            System.out.println(dependingMap.getOrDefault(type, 0) + " " + type);
+            for (ItemCollectionType depending : ItemCollectionType.values()) {
+                if (!depending.dependencies.contains(type)) continue;
+                System.out.println("  └─ " + depending);
+            }
+        }
+    }
+}
