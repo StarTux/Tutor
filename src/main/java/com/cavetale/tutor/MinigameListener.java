44e08f91b572ebde7bfb4127316520e4719e8bf6
StarTux <ths.loy@gmail.com>
2024-01-29 19:20:51 +0100

    Nerf the kitty con chances, especially of winner teams

--- src/main/java/com/cavetale/tutor/MinigameListener.java
+++ src/main/java/com/cavetale/tutor/MinigameListener.java
@@ -5,6 +5,7 @@ import com.cavetale.inventory.mail.ItemMail;
 import com.cavetale.mytems.Mytems;
 import java.util.List;
 import java.util.UUID;
+import java.util.concurrent.ThreadLocalRandom;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -22,16 +23,27 @@ public final class MinigameListener implements Listener {
     private void onMinigameMatchComplete(MinigameMatchCompleteEvent event) {
         if (!event.getType().isPubliclyAvailable()) return;
         if (event.getPlayerUuids().size() < 3) return;
+        final int chance = event.getWinnerUuids().size();
         for (UUID player : event.getPlayerUuids()) {
             if (event.getWinnerUuids().contains(player)) {
+                final boolean prime = chance <= 1
+                    || ThreadLocalRandom.current().nextInt(chance) == 0;
                 // Winner
                 ItemMail.send(player,
-                              List.of(Mytems.KITTY_COIN.createItemStack()),
+                              List.of(prime
+                                      ? (ThreadLocalRandom.current().nextBoolean()
+                                         ? Mytems.KITTY_COIN.createItemStack()
+                                         : Mytems.DIAMOND_COIN.createItemStack())
+                                      : (ThreadLocalRandom.current().nextBoolean()
+                                         ? Mytems.RUBY.createItemStack(3)
+                                         : Mytems.DIAMOND_COIN.createItemStack())),
                               text("Winning a game of " + event.getType().getDisplayName(), GOLD));
             } else {
                 // Regular Player
                 ItemMail.send(player,
-                              List.of(Mytems.RUBY.createItemStack()),
+                              List.of(ThreadLocalRandom.current().nextBoolean()
+                                      ? Mytems.RUBY.createItemStack()
+                                      : Mytems.GOLDEN_COIN.createItemStack()),
                               text("Playing a game of " + event.getType().getDisplayName(), GREEN));
             }
         }
